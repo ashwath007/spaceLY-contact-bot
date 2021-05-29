0dc1c9b7f2a82f3cd811fb3950ce6f576e75b6c7
ashwath007 <srivicky2000@gmail.com>
2021-05-29 22:29:19 +0530

    SpaceL Y Dash wok

--- spacely-support/src/index.js
+++ spacely-support/src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Routes from "./Routes"
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes/>
   </React.StrictMode>,
   document.getElementById('root')
 );
